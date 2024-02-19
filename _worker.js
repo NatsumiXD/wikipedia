ceb43406918288f2e79cd5edfc7b57d8e1483ebb
Roren Natsumi <63963655+NatsumiXD@users.noreply.github.com>
2024-02-19 20:35:10 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,10 @@
 export default {
   async fetch(request, env) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'https://zh.wikipedia.org'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
-    return env.ASSETS.fetch(request);
+    const _url = new URL(request.url);
+    _url.hostname = _url.pathname.startsWith("/gh/")
+      ? "cdn.jsdelivr.net"
+      : "zh.wikipedia.org";
+    const req = new Request(_url, request);
+    return fetch(req);
   },
 };
